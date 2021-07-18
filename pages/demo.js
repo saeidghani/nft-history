3e6a5cbe5f9336ac2191577afd741deabaa53450
saeid <saeidghani@gmail.com>
2021-07-18 13:25:59 +0430

    removed demo page

--- pages/demo.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import BidCard from '../../components/common/BidCard';
-import CalendarSlider from '../../components/common/CalendarSlider';
-import Layout from '../../layout';
-
-export default function Home() {
-  return <Layout></Layout>;
-}
